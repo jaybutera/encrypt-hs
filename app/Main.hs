8bc4c7a962f3e30818e57682572e521766a67b4f
Jay Butera <buterajay@protonmail.com>
2020-10-29 22:56:54 -0500

    Run encryption on all nodes of a directory

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,19 @@ module Main where
 
 import Lib
 import Crypto.Hash
+import System.Directory (listDirectory)
 import Data.ByteString (ByteString, empty)
-import System.IO (openFile, hClose, IOMode( ReadMode ))
+import System.IO (withFile, openFile, hClose, IOMode( ReadMode ))
 import qualified Data.ByteString (getLine, hGetContents)
 
-exampleHashWith :: ByteString -> IO ()
-exampleHashWith msg = do
-    putStrLn $ "  sha1(" ++ show msg ++ ") = " ++ show (hashWith SHA1   msg)
-    putStrLn $ "sha256(" ++ show msg ++ ") = " ++ show (hashWith SHA256 msg)
+getContent :: String -> IO ByteString
+-- use withBinaryFile for images?
+getContent file = withFile file ReadMode Data.ByteString.hGetContents
 
-main :: IO ()
+main :: IO [()]
 main = do
-    --msg <- empty :: ByteString
-    --msg <- Data.ByteString.getLine
-    handle <- openFile "package.yaml" ReadMode
-    msg <- Data.ByteString.hGetContents handle
-    hClose handle
-    exampleAES256 msg
+    file_list <- listDirectory "."
+    --exampleAES256 <$> mapM getContent file_list
+    -- check if is file with doesFileExist
+    contents <- mapM getContent file_list
+    mapM exampleAES256 contents
